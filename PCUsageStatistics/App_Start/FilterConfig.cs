b9912b694a7c850113248c5711d7873c430a5a90
sajidhanif77 <sajidhanif77@outlook.com>
2016-12-16 23:55:03 +0100

    Add project files.

---
+++ PCUsageStatistics/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PCUsageStatistics
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PCUsageStatistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PCUsageStatistics.Startup))]
+namespace PCUsageStatistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
